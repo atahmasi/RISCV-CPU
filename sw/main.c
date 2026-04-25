2807d4773614e791dadcfd3615b0c56d317a17cd
Arthur T <atahmasi@uwaterloo.ca>
2026-04-25 15:01:00 -0400

    debug

--- sw/main.c
+++ sw/main.c
@@ -1,26 +1,22 @@
 #define LED_REG (*(volatile unsigned int *)0x80000000)
 
-void delay() {
-    for(volatile int i = 0; i < 5000000; i++);
-}
-
 int main() {
-    LED_REG = 5;        // expect 00000101
-    delay();
+    LED_REG = 5;
+    for(volatile int i = 0; i < 5000000; i++);
 
-    LED_REG = 5 + 3;    // expect 00001000
-    delay();
+    LED_REG = 8;
+    for(volatile int i = 0; i < 5000000; i++);
 
-    LED_REG = 5 - 3;    // expect 00000010
-    delay();
+    LED_REG = 2;
+    for(volatile int i = 0; i < 5000000; i++);
 
-    LED_REG = 5 & 3;    // expect 00000001
-    delay();
+    LED_REG = 1;
+    for(volatile int i = 0; i < 5000000; i++);
 
-    LED_REG = 5 | 3;    // expect 00000111
-    delay();
+    LED_REG = 7;
+    for(volatile int i = 0; i < 5000000; i++);
 
-    LED_REG = 0xFF;     // expect 11111111
+    LED_REG = 0xFF;
     while(1);
     return 0;
 }
