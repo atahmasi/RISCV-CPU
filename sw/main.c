ee1467b8e3a0cd45b1db5bc4fceb0d8e7cdc6e60
Arthur T <atahmasi@uwaterloo.ca>
2026-04-25 15:06:34 -0400

    deb

--- sw/main.c
+++ sw/main.c
@@ -1,20 +1,22 @@
 #define LED_REG (*(volatile unsigned int *)0x80000000)
 
+volatile int i;
+
 int main() {
     LED_REG = 5;
-    for(volatile int i = 0; i < 5000000; i++);
+    for(i = 0; i < 5000000; i++);
 
     LED_REG = 8;
-    for(volatile int i = 0; i < 5000000; i++);
+    for(i = 0; i < 5000000; i++);
 
     LED_REG = 2;
-    for(volatile int i = 0; i < 5000000; i++);
+    for(i = 0; i < 5000000; i++);
 
     LED_REG = 1;
-    for(volatile int i = 0; i < 5000000; i++);
+    for(i = 0; i < 5000000; i++);
 
     LED_REG = 7;
-    for(volatile int i = 0; i < 5000000; i++);
+    for(i = 0; i < 5000000; i++);
 
     LED_REG = 0xFF;
     while(1);
