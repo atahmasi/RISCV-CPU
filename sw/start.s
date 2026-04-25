8fb7f3b42a7800686e46b6f5a1be7bb8d9eddd72
Arthur T <atahmasi@uwaterloo.ca>
2026-04-25 19:56:38 -0400

    debug

--- sw/start.s
+++ sw/start.s
@@ -2,7 +2,7 @@
 .global _start
 
 _start:
-    li sp, 0x1800    # top of dmem (0x400 + 0x1000 = 0x1800)
+    li sp, 0x1800    # top of dmem (0x800 + 0x1000 = 0x1800)
     call main
 halt:
     j halt
